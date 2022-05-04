8cefe89bae26f840c2164d26975060c0b12e9693
EkaterinaShamshina <nireta2009@yandex.ru>
2022-05-04 10:40:55 +0300

    smooth scroll and id for links were added

--- src/index.js
+++ src/index.js
@@ -87,3 +87,19 @@ var swiper = new Swiper(".offerSwiper", {
       }
     }
   });
+
+  // Smooth Scroll
+  const links = document.querySelectorAll(".link");
+
+  for (const link of links) {
+      link.addEventListener("click", clickHandler);
+  }
+
+  function clickHandler(e) {
+      e.preventDefault();
+      const href = this.getAttribute("href");
+
+      document.querySelector(href).scrollIntoView({
+      behavior: "smooth"
+      });
+      }
